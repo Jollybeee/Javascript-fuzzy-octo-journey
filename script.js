8babbd4982051c4587dc6e7c2718fe68e10c3c3a
Adesola Felix <felixadesola9@gmail.com>
2023-07-28 13:37:10 +0100

    newbie

---
+++ script.js
@@ -0,0 +1,15 @@
+let message = ('good night');
+console.log(mesage.length);
+
+let age = (7);
+
+let mine =('tommy is ' + age + ' years old');
+console.log(mine);
+
+let array = ('The men at the radio station called the fire station');
+
+console.log(array);
+
+console.log(age);
+
+let area = (100);
---
+++ scripted.js
@@ -0,0 +1,35 @@
+<script>
+   {/* document.querySelectorAll('.bth').forEach((item) => {
+        item.addEventListener('click',  () => {
+            alert('this is bold')
+        })
+    })
+}
+
+ // for loops
+    for(let i = 0; i < 5; i++){
+        console.log('in loop:', i)         
+
+    }
+    console.log('loop finished') 
+}*/
+   /* const name = ['shaun', 'mario', 'luigi'];
+
+    for(let i = 0; i < name.length; i++){
+       // console.log(names[i]);
+       let html = `<div>${names[i]}</div>`;
+       console.log(html);
+    }
+    
+   let i = 0;
+   while(i < 5){
+    console.log('in loop: ', i);
+    i++ 
+   }
+let i = 0;
+while(i < name.length){
+console.log(name[i]);
+i++;
+}
+*/
+</script>
